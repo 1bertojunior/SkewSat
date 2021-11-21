0478a7596e1ba24550f41e133ec620b32e976e1b
Emanuel <emanuelmiranda932@gamil.com>
2021-11-21 12:05:03 -0300

    classe para mapa

--- src/SkewSat/SkewSat.py
+++ src/SkewSat/SkewSat.py
@@ -5,14 +5,15 @@ import math
 
 class SkewSat():
 
-    def __init__(self,rx_lat:float, rx_log:float,tx_lon:float) -> None:
+    def __init__(self,rx_lat:float, rx_log:float,tx_log:float) -> None:
         self._rx_log = rx_log # LOGITUDE DA ANTENA
         self._rx_lat = rx_lat # LATITUDE DA ANTENA
-        self._tx_lon = tx_lon # LOGITUDE DO SATÉLITE
+        self._tx_log = tx_log # LOGITUDE DO SATÉLITE
         self._tx_lat = 0      # LATITUDE DA SATÉLITE
 
         self._R0 = 6.371      # RAIO DA TERRA
         self._h_tx = 35.786   # ALTURA DO SATÉLITE
+        self.map = folium.Map(location=[rx_lat, rx_log], zoom_start=15)
 
     
     def checkValues(self) -> int:
@@ -31,7 +32,7 @@ class SkewSat():
         # Insira a longitude do satélite
         # no (formato decimal, positivo = leste, negativo = oeste,
         # valores inválidos entre -180 e 180 graus).
-        if self._tx_log == 0 | self._tx_lon < -180 | self._tx_lon > 180:
+        if self._tx_log == 0 | self._tx_log < -180 | self._tx_log > 180:
             return -3
 
         # TODAS AS COORDENADAS ESTÃO CORRETAS
@@ -40,14 +41,14 @@ class SkewSat():
 
     # DELTA = RX_LOGITUDE - TX_LOGITUDE
     def getDelta(self) -> float:
-        return self._rx_log - self._tx_lon
+        return self._rx_log - self._tx_log
 
 
     # CALCULAR AZIMUTE 
     def getAzimuth(self) -> float:
         az = 0
         
-        delta = self._rx_log - self._tx_lon
+        delta = self._rx_log - self._tx_log
 
         az = math.atan(
            (
@@ -65,7 +66,7 @@ class SkewSat():
 
     def getElevation(self) -> float:
         el = 0
-        delta = self._rx_log - self._tx_lon
+        delta = self._rx_log - self._tx_log
 
         num = (
             math.cos(rx_lat*(math.pi/180)) *
@@ -86,6 +87,36 @@ class SkewSat():
 
         return el
 
+    def to_point(self, msg='', msg_preview='Mais detalhes!', clr='red', zoom_start=15):
+        azimuth = self.getAzimuth()  # AZIMUTE VERDADEIRO
+        elevation = self.getElevation()  # ELEVAÇÃO DE ANTENA
+        elevation_offset = elevation - 20.5
+        msg = f'''Rx latitude: {self._rx_lat}\nRx longitude: {self._rx_log}\nSatelite: {self._tx_log}
+            Elevação: {elevation:.1f}º\nElevação Offset: {elevation_offset:.1f}º\nAzimuth Verdadeiro: {azimuth:.1f}º'''
+        folium.Marker([self._rx_lat, self._rx_log],
+                      zoom_start=zoom_start,
+                      popup=f'<i>{msg}</i>',
+                      tooltip=msg_preview,
+                      icon=folium.Icon(color=clr)).add_to(self.map)
+
+    def open_map(self, path='SkewSatMap.html'):
+        html_page = f'{path}'
+        self.map.add_child(folium.LatLngPopup())
+        self.map.save(html_page)
+        # open in browser.
+        new = 2
+        webbrowser.open(html_page, new=new)
+
+    def set_row_for_satellite(self, color='red'):
+
+        points_rx = [self._rx_lat, self._rx_log]
+        points_tx = [self._tx_lat, self._tx_log]
+
+        points = [points_rx, points_tx]
+
+        folium.PolyLine(points, color=color).add_to(self.map)
+
+
 if __name__ == '__main__':
 
     rx_lat = -7.3758454
@@ -101,3 +132,7 @@ if __name__ == '__main__':
     print( f'Azimute verdadeiro: {azimuth:.1f}°')
     print( f'Elevação: {elevation:.1f}°')
     print( f'Elevação (offset): {elevation_offset:.1f}°')
+
+    sk.to_point()
+    sk.set_row_for_satellite()
+    sk.open_map()
